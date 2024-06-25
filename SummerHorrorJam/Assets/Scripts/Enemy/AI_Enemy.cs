c90b47124aba929e003101f40b285c3a106f2989
izansl <125253405+izansl@users.noreply.github.com>
2024-06-25 13:09:47 +0200

    Cambios enemigo

--- SummerHorrorJam/Assets/Scripts/Enemy/AI_Enemy.cs
+++ SummerHorrorJam/Assets/Scripts/Enemy/AI_Enemy.cs
@@ -5,22 +5,60 @@ using UnityEngine.AI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public Transform Objetivo;
+    public Transform Objetivo_1;
+    public Transform Objetivo_2;
+    public Transform Objetivo_3;
+    Transform Objetivo = null;
+    bool Objetivoalcanzado = false;
+    public Transform Player;
     public float Vel;
     public NavMeshAgent IA;
     public float Distance;
+    int random;
 
     void Update()
     {
         IA.speed = Vel;
-        if (Vector3.Distance(transform.position, Objetivo.position) < Distance)
+        if (Vector3.Distance(transform.position, Player.position) < Distance)
         {
-            IA.SetDestination(Objetivo.position);
+            IA.SetDestination(Player.position);
         }
         else
         {
-            IA.SetDestination(transform.position);
+            Randomize();
         }
 
     }
+    void Randomize()
+    {
+        if(Objetivoalcanzado == true)
+        {
+            random = Random.Range(1, 4);
+            Objetivoalcanzado = false;
+        }
+        switch (random)
+        {
+            case 1:
+                Objetivo = Objetivo_1;
+                break;
+            case 2:
+                Objetivo = Objetivo_2;
+                break;
+            case 3:
+                Objetivo = Objetivo_3;
+                break;
+        }
+        IA.SetDestination(Objetivo.position);
+        if(Vector3.Distance(transform.position, Objetivo.position) < 5)
+        {
+            Objetivoalcanzado = true;
+        }
+    }
 }
+
+
+
+
+
+
+
