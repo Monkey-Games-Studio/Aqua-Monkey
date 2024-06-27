240d4dbab03c8cdebd8587ea5ad6aa01997ddbb3
izansl <125253405+izansl@users.noreply.github.com>
2024-06-27 11:45:21 +0200

    Codigo llave

--- SummerHorrorJam/Assets/Scripts/Llave_puerta/Llave_puerta.cs
+++ SummerHorrorJam/Assets/Scripts/Llave_puerta/Llave_puerta.cs
@@ -1,37 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Windows;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Llave_puerta : MonoBehaviour
 {
     public Transform llave;
-    public Transform puerta;
-    public Transform panel;
-    public Transform player;
 
-    bool llave_cogida = false;
-    bool panel_abierto = false;
-    void Start()
+    public bool llaveobtenida = false;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            Obtener();
+        }
     }
 
-    void Update()
+    private void Obtener()
     {
-        if (llave.GetComponent<Collider>().isTrigger == true && player.GetComponent<Collider>().isTrigger == true && Input.GetKey(KeyCode.F))
-        {
-            Destroy(llave.gameObject);
-            llave_cogida = true;
-        }
-
-        if (llave_cogida == true && panel.GetComponent<Collider>().isTrigger == true && Input.GetKey(KeyCode.F))
-        {
-            panel_abierto = true;
-        }
-
-        if (panel_abierto == true && puerta.GetComponent<Collider>().isTrigger == true)
-        {
-            puerta.position = Vector3.MoveTowards(puerta.position, new Vector3(puerta.position.x, puerta.position.y + 5, puerta.position.z), 0.1f);
-        }
+        llaveobtenida = true;
+        Destroy(llave.gameObject);
     }
 }
