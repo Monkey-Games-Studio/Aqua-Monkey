5bb51c1a31525b76a084dcd1b71f0bce56fd9a4a
izansl <125253405+izansl@users.noreply.github.com>
2024-06-28 11:42:32 +0200

    Cambios puerta

--- SummerHorrorJam/Assets/Scripts/Llave_puerta/Puerta.cs
+++ SummerHorrorJam/Assets/Scripts/Llave_puerta/Puerta.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Puerta : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform puerta;
+    public Transform panel;
+
+    private bool puertaSubida = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player") && panel.GetComponent<Panel>().panelActivado && !puertaSubida)
+        {
+            SubirPuerta();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (puertaSubida)
+        {
+            BajarPuerta();
+        }
+    }
+
+    private void SubirPuerta()
+    {
+        puerta.position += new Vector3(0, 5, 0);
+        puertaSubida = true;
+    }
+
+    private void BajarPuerta()
+    {
+        puerta.position -= new Vector3(0, 5, 0);
+        puertaSubida = false;
     }
 }
